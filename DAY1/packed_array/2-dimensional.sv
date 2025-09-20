3a770b2934c0a218b3e2fadef2663a858c85e514
ARCHANA A <archanaarumugam0017@gmail.com>
2025-09-20 15:21:22 +0530

    Create 2-dimensional.sv

---
+++ DAY1/packed_array/2-dimensional.sv
@@ -0,0 +1,10 @@
+module two_d_packed_array();
+bit[3:0][7:0]m_data;
+initial begin
+m_data = 32'h 01020304;
+$display ("m_data 0x%h ",m_data);
+for (int i=0; i<4; i++) begin
+$display ("data[%0d] = 0x%0h",i,m_data[i]);
+end
+end
+endmodule
