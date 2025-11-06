02e7cc427b43807b0e447c05ba9c5a420186f940
ARCHANA A <archanaarumugam0017@gmail.com>
2025-11-06 17:00:51 +0530

    Create assigning child class handle to parent class handle.sv

---
+++ DAY_18/dynamic casting/assigning child class handle to parent class handle.sv
@@ -0,0 +1,26 @@
+class parent_class;
+  bit [31:0] addr;
+  function display();
+    $display("Addr = %0d",addr);
+  endfunction
+endclass
+
+class child_class extends parent_class;
+  bit [31:0] data;
+  function display();
+    super.display();
+    $display("Data = %0d",data);
+  endfunction
+endclass
+module testbench();
+  initial begin
+  parent_class p;
+  child_class  c;
+  p=new();
+  c=new();
+    c.addr=10;
+    c.data=20;
+    p=c;
+    c.display();
+  end
+endmodule
