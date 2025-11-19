9f6aa09bb0cf203d5f5d66e7739e1bf53e8f4f7d
ARCHANA A <archanaarumugam0017@gmail.com>
2025-11-19 10:33:53 +0530

    Add SystemVerilog class and testbench for constraints

---
+++ DAY_25/constraint_example.sv
@@ -0,0 +1,15 @@
+class constraint_blocks;
+   rand bit[3:0]addr;
+  constraint addr_range{addr<5;}
+endclass
+module constraint_tb;
+  constraint_blocks pkt;
+  //pkt=new();
+  initial begin
+    pkt=new();
+    repeat(10)begin
+      pkt.randomize();
+      $display("values randomized=%0d",pkt.addr);
+    end
+  end
+endmodule
