44561b46b839662339acee7ac380f8f0bbb90fd2
ARCHANA A <archanaarumugam0017@gmail.com>
2025-11-20 10:41:33 +0530

    Create implication constraint.sv

---
+++ DAY_26/implication constraint.sv
@@ -0,0 +1,26 @@
+class sequential_item;
+  rand bit[7:0]value;
+  rand enum{LOW,HIGH}scale;
+  
+ //constraint scale_tr{if(scale==LOW)
+                         // value<20;
+                      //else if(scale==MID)
+                      //{value<=30;value>=50;}
+                        //else value >70;}
+  constraint scale_c { (scale == LOW) -> value <50; }
+
+endclass
+module seq_tb;
+  sequential_item item;
+  
+  initial begin
+    item=new();
+    repeat(10)begin
+      item.randomize();
+      $display("scale=%s,value=%0d",item.scale.name(),item.value);
+    end
+  end
+endmodule
+                        
+                      
+  
